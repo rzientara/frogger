2a98dd7c3323ee2a92793ae5c2a808cc6c8e4570
rzientara <46692302+rzientara@users.noreply.github.com>
2020-04-20 14:45:52 -0500

    reformat and rebalance
    moved high score coding to functions
    adjusted car speed

--- frogger.py
+++ frogger.py
@@ -116,36 +116,38 @@ class Car:
         self.name.dx = self.speed
 
 # car 1
-car_1 = Car("car1", "pictures/car1.gif", "pictures/car1.2.gif", 1, -150, 62)
+car_1 = Car("car1", "pictures/car1.gif", "pictures/car1.2.gif", .5, -150, 62)
 car_1.set_car()
 
 # car 2
-car_2 = Car("car2", "pictures/car2.gif", "pictures/car2.2.gif", .8, -100, 50)
+car_2 = Car("car2", "pictures/car2.gif", "pictures/car2.2.gif", .3, -100, 50)
 car_2.set_car()
 
 # car 3
-car_3 = Car("car3", "pictures/car3.gif", "pictures/car3.2.gif", 1.5, -50, 58)
+car_3 = Car("car3", "pictures/car3.gif", "pictures/car3.2.gif", .7, -50, 58)
 car_3.set_car()
 
 # car 4
-car_4 = Car("car4", "pictures/car4.gif", "pictures/car4.2.gif", .9, 0, 77)
+car_4 = Car("car4", "pictures/car4.gif", "pictures/car4.2.gif", .45, 0, 77)
 car_4.set_car()
 
 # car 5
-car_5 = Car("car5", "pictures/bus1.gif", "pictures/bus1.2.gif", .8, 50, 102)
+car_5 = Car("car5", "pictures/bus1.gif", "pictures/bus1.2.gif", .35, 50, 102)
 car_5.set_car()
 
 # car 6
-car_6 = Car("car6", "pictures/truck1.gif", "pictures/truck1.2.gif", .7, 100, 85)
+car_6 = Car("car6", "pictures/truck1.gif", "pictures/truck1.2.gif", .4, 100, 85)
 car_6.set_car()
 
 # car 7
-car_7 = Car("car7", "pictures/car5.gif", "pictures/car5.2.gif", .9, 150, 69)
+car_7 = Car("car7", "pictures/car5.gif", "pictures/car5.2.gif", .55, 150, 69)
 car_7.set_car()
 
 # car 8
-car_8 = Car("car8", "pictures/bus2.gif", "pictures/bus2.2.gif", 1, 200, 87)
+car_8 = Car("car8", "pictures/bus2.gif", "pictures/bus2.2.gif", .6, 200, 87)
 car_8.set_car()
+
+cars = [car_1, car_2, car_3, car_4, car_5, car_6, car_7, car_8]
 
 ###################
 # functions
@@ -229,8 +231,8 @@ def open_menu():
     start_pen.write("Start", align="center", font=("Courier", 24, "bold"))
     exit_pen.write("Exit", align="center", font=("Courier", 24, "normal"))
 
-    wn.onkey(menu_up, "Up")
-    wn.onkey(menu_down, "Down")
+    wn.onkey(menu_up, "w")
+    wn.onkey(menu_down, "s")
     wn.onkey(exit_menu, "space")
 
     global at_menu
@@ -258,9 +260,10 @@ def select_letter():
 
     letter_amount += 1
     letter_number = 0
-def show_car(car):
-    car.name.showturtle()
+
+def move_car(car):
     car.name.setx(car.name.xcor() + car.name.dx)
+
 def reset_car(car, random_side):
     if car.name.xcor() < -650 or car.name.xcor() > 650:
         side = random.choice(random_side)
@@ -272,6 +275,25 @@ def reset_car(car, random_side):
             car.name.shape(car.leftward)
         if score > 7:
             change_speed(car.name)
+
+def clear_game():
+    wn.bgpic("nopic")
+    wn.bgcolor("black")
+    score_pen.clear()
+    lives_pen.clear()
+    frog.hideturtle()
+    for i in cars:
+        i.name.hideturtle()
+        i.name.goto(650, i.ypos)
+
+def close_high_score(number, amount, score, lives):
+    letter_number = 0
+    letter_amount = 0
+    title_pen.clear()
+    name_pen.clear()
+    score = 0
+    lives = 3
+    return False
 
 # start at menu
 wn.listen()
@@ -294,61 +316,60 @@ while True:
         score_pen.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
 
     # move car 1
-    show_car(car_1)
+    car_1.name.showturtle()
+    move_car(car_1)
     reset_car(car_1, random_side)
 
     # display and move car 2
     if score > 0:
-        show_car(car_2)
+        car_2.name.showturtle()
+        move_car(car_2)
         reset_car(car_2, random_side)
 
     # display and move car 3
     if score > 1:
-        show_car(car_3)
+        car_3.name.showturtle()
+        move_car(car_3)
         reset_car(car_3, random_side)
 
     # display and move car 4
     if score > 2:
-        show_car(car_4)
+        car_4.name.showturtle()
+        move_car(car_4)
         reset_car(car_4, random_side)
 
     # display and move car 5
     if score > 3:
-        show_car(car_5)
+        car_5.name.showturtle()
+        move_car(car_5)
         reset_car(car_5, random_side)
 
     # display and move car 6
     if score > 4:
-        show_car(car_6)
+        car_6.name.showturtle()
+        move_car(car_6)
         reset_car(car_6, random_side)
 
     # display and move car 7
     if score > 5:
-        show_car(car_7)
+        car_7.name.showturtle()
+        move_car(car_7)
         reset_car(car_7, random_side)
 
     # display and move car 8
     if score > 6:
-        show_car(car_8)
+        car_8.name.showturtle()
+        move_car(car_8)
         reset_car(car_8, random_side)
 
     # hit detection
-    cars = [car_1, car_2, car_3, car_4, car_5, car_6, car_7, car_8]
-
     for i in cars:
         if i.name.xcor() - i.hitbox < frog.xcor() < i.name.xcor() + i.hitbox and i.name.ycor() + 35 > frog.ycor() > i.name.ycor() - 45:
             lives = lose_life(lives)
 
     # if out of lives, clear game screen
     if lives < 0:
-        wn.bgpic("nopic")
-        wn.bgcolor("black")
-        score_pen.clear()
-        lives_pen.clear()
-        frog.hideturtle()
-        for i in cars:
-            i.name.hideturtle()
-            i.name.goto(650, i.ypos)
+        clear_game()
 
         # disable frog key
         wn.onkey(None, "w")
@@ -359,8 +380,8 @@ while True:
             wn.update()
             if letter_amount < 3:
                 # keys for name entry
-                wn.onkey(letter_up, "Up")
-                wn.onkey(letter_down, "Down")
+                wn.onkey(letter_up, "w")
+                wn.onkey(letter_down, "s")
                 wn.onkey(select_letter, "space")
 
                 letters_chosen[letter_amount] = letters[letter_number]
@@ -368,13 +389,6 @@ while True:
                 name_pen.clear()
                 name_pen.write("{}{}{}".format(letters_chosen[0], letters_chosen[1], letters_chosen[2]), align="center", font=("Courier", 24, "normal"))
             else:
-                letter_number = 0
-                letter_amount = 0
-                title_pen.clear()
-                name_pen.clear()
-                high_score_s = False
-
+                high_score_s = close_high_score(letter_number, letter_amount, score, lives)
         # reset score and lives
-        score = 0
-        lives = 3
         open_menu()
